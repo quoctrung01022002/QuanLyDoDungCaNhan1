6e0897b635fdd0d7ddbb653416af86af54f6a956
trung <119313108+quoctrung01022002@users.noreply.github.com>
2023-12-05 17:14:29 +0700

    Delete TranQuocTrung_62132908.62.CNTT-3 directory

--- TranQuocTrung_62132908.62.CNTT-3/TranQuocTrung_62132908.62.CNTT-3/Repository/IHoaDonBanRepositpry.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TranQuocTrung_62132908._62.CNTT_3.Repository
-{
-    public class IHoaDonBanRepositpry
-    {
-    }
-}
--- TranQuocTrung_62132908.62.CNTT-3/TranQuocTrung_62132908.62.CNTT-3/Service/HoaDonBanService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TranQuocTrung_62132908._62.CNTT_3.Service
-{
-    public class HoaDonBanService
-    {
-    }
-}
